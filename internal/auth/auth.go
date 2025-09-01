691140cea027fb32b5c14a99abc2751b5c80f778
Andres Tellez <andres.tezper@gmail.com>
2025-09-01 00:26:28 -0600

    feat: refactor folder structure - new backend folder

--- internal/auth/auth.go
+++
@@ -1,7 +0,0 @@
-package auth
-
-import "github.com/golang-jwt/jwt/v5"
-
-type Authenticator interface {
-	GenerateToken(claims jwt.Claims) (string, error)
-}
--- scripts/db_init.sql
+++
@@ -1,6 +0,0 @@
--- This script is used to reference some of the 
--- commands and extensions used in DB migrations.
-
-CREATE EXTENSION IF NOT EXISTS citext;
-
-CREATE DATABASE teo5;
